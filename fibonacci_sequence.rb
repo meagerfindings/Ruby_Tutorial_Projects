08e9ba78fc14c0865d006387fd6e295eb42724d9
Mat Greten <mgreten@gmail.com>
2017-04-25 15:04:32 -0600

    Add files via upload

---
+++ fibonacci_sequence.rb
@@ -0,0 +1,19 @@
+puts "\nPlease input an integer representing how many times you want to run the Fibbonacci sequence: \n"
+
+iterationNumberInput = gets.chomp.to_i
+
+fibNum = 0
+fibOne = 0
+fibTwo = 1
+
+i = 0
+while i < iterationNumberInput do
+  fibNum = fibOne + fibTwo
+  fibOne = fibTwo
+  fibTwo = fibNum
+
+  i += 1
+end
+
+puts "\nFibbonacci nubmer: "
+puts fibNum
